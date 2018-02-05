ab7af9cd809256e4f8ba20f484d12ef9c359505c
ahmedezzat85 <ahmedezzateasa@gmail.com>
2018-02-05 09:51:29 +0200

    Create example submission

---
+++ Code/image_preprocessing.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+
+_R_MEAN = 123.68
+_G_MEAN = 116.78
+_B_MEAN = 103.94
+_RGB_MEAN = np.array([_R_MEAN, _G_MEAN, _B_MEAN]) / 255
+
+
+def _resize_keep_aspect(image, short_side=256, img_size=224):
+    """ Perform Resize for the input image while preserving 
+    the aspect ratio. The short edge of the image is set to 
+    be equal to r'self.rescale_size' and the other dimension
+    is scaled accordingly to keep the aspect ratio as is.  
+    """
+    # Resize
+    h, w, _ = image.shape
+    scale = short_side / min(h, w)
+    h = int(h * scale)
+    w = int(w * scale)
+    image = cv2.resize(image, (w, h))
+    
+    # Center Crop to the target size 
+    hs = (h - img_size) // 2
+    ws = (w - img_size) // 2
+    image = image[hs:hs+img_size, ws:ws+img_size]
+    return image
+
+def preprocess_image(image, short_side=256, img_size=224, preserve_aspect=True):
+    """ """
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image.astype(np.float32)
+    image = image / 255
+
+    if preserve_aspect is True:
+        image = _resize_keep_aspect(image)
+    else:
+        image = cv2.resize(image, (img_size, img_size))
+
+    image = image - _RGB_MEAN
+    return np.expand_dims(image, 0)
--- scripts/mv_submit.sh
+++ scripts/mv_submit.sh
@@ -2,24 +2,43 @@
 
 CWD=$PWD
 MODEL=$1
-INPUT_SIZE=$2
+IN_SZ=$2
 DATASET_KEY=$3
+MODEL_DIR=../bin/$MODEL
+MODEL_FILE=$MODEL_DIR/network.meta
 OUT_DIR=$PWD/../submission
+COMPILED_GRAPH=compiled.graph
+
 # TODO: Check inputs
 # TODO: password automation
 
 # Create directory for submission
+sudo rm $COMPILED_GRAPH
 sudo rm $OUT_DIR -R
 mkdir $OUT_DIR
 sudo chmod 777 $OUT_DIR -R
+cd $OUT_DIR
+
+# Copy executable scripts
+cp ../Code/mvncs_inference.py .
+cp ../Code/base_inference.py .
+cp ../Code/image_preprocessing.py .
 
 # Generate NCS Compiled Model
-./ncs_graph_gen.sh $MODEL $INPUT_SIZE
-mv compiled.graph $OUT_DIR
+echo 'Generating Graph For Model for "'$MODEL  $IN_SZ'"'
+cp $MODEL_DIR/network.* .
+mvNCCompile network.meta -s 12 -in input -on output -o $COMPILED_GRAPH -is $IN_SZ $IN_SZ
 
-cd $OUT_DIR
-cp ../Code/mvncs_inference.py .
+# Run inference
 python3 mvncs_inference.py -d $DATASET_KEY -s
+
+mkdir supporting
+sudo chmod 777 supporting -R
+mv mvncs_inference.py supporting/
+mv base_inference.py supporting/
+mv image_preprocessing.py supporting/
+cp ../scripts/README supporting/
+cp ../dataset/eval_set.csv supporting/
 
 cd $PWD
 echo 'DONE...'
