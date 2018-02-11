0f484b57590a8d4f9fd51704ade50e5feb23ad30
ahmedezzat85 <ahmedezzateasa@gmail.com>
2018-02-11 20:46:56 +0200

    add script to download mobilenet pretrained models

---
+++ scripts/get_mobilenet.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+Download_DIR=$PWD/../Code/model/mobilenet_v1
+
+mkdir $Download_DIR
+sudo chmod 777 $Download_DIR
+cd $Download_DIR
+
+wget http://download.tensorflow.org/models/mobilenet_v1_1.0_224_2017_06_14.tar.gz
+wget http://download.tensorflow.org/models/mobilenet_v1_1.0_192_2017_06_14.tar.gz
+wget http://download.tensorflow.org/models/mobilenet_v1_0.75_224_2017_06_14.tar.gz
+wget http://download.tensorflow.org/models/mobilenet_v1_0.75_192_2017_06_14.tar.gz
+
+tar -xzf *.tar.gz
+
+cd $PWD
